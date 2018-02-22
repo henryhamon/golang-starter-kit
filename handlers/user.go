a87849297bec0b8c25024098a56c4b20d00c47bb
henryhamon <henry@hamonrails.com.br>
2018-02-22 15:30:33 -0300

    I don't know how to test handlers in golang

--- handlers/user.go
+++ handlers/user.go
@@ -1,20 +1,28 @@
 package handlers
 
 import (
-	"fmt"
-	dbpkg "golang-starter-kit/db"
-	"golang-starter-kit/models"
 	"net/http"
+
+	dbpkg "golang-starter-kit/db"
+	"golang-starter-kit/helper"
+	"golang-starter-kit/models"
 
 	"github.com/gin-gonic/gin"
 )
 
 func GetUsers(c *gin.Context) {
 	db := dbpkg.DBInstance(c)
-	users := []models.User{}
-	db.Find(&users)
+	parameter, err := helper.NewParameter(c)
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
 
-	fmt.Println("aki")
+	users, err := models.ListUsers(db, parameter)
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
 
 	c.JSON(http.StatusOK, gin.H{"users": users})
 }
